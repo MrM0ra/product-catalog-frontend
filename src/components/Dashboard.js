cb2cfbd08fd8e32481fdf3d291ab54498295be3f
Victor Esteban Mora Arce <themonstermora@gmail.com>
2022-03-23 00:00:44 -0500

    error mesasge showing when no products hav ebeen created by the logged in user

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
+import { UserContext } from '../context/UserContext';
 
 const Dashboard = (props) => {
+
+    const {userId} = useContext(UserContext);
+
+    const [products, setProducts] = useState([]);
+
+	useEffect( () => {
+		fetch(`http://127.0.0.1:8080/product/user/${JSON.parse(localStorage.getItem('user')).userId}`)
+		.then(res => res.json())
+		.then((result) => {
+			setProducts(result);
+		}
+		)},[]);
+
   return (
-    <div>Dashboard</div>
+    <div>
+        <h2>Dashboard</h2>
+        <div>
+            {
+                products.length<=0  ? 
+                <div>
+				    <img src="https://cdn.iconscout.com/icon/free/png-256/data-not-found-1965034-1662569.png" alt=""></img>
+				    <p>It seems like you haven't created any products yet</p>
+				    <input onClick={() => props.changePage("AddTopic")} type="button" value="Add New Topic"></input>
+			    </div>
+                :
+                <div>
+                    productos creados por el Usuario
+                </div>
+            }
+        </div>
+    </div>
   )
 }
 
